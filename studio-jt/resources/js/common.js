f16a71f7d8426b07e0ee212e3a3b95f425e26c97
simbyungki <dung8524@gmail.com>
2019-08-26 14:49:37 +0900

    update

--- studio-jt/resources/js/common.js
+++ studio-jt/resources/js/common.js
@@ -1,4 +1,29 @@
 $(function(){
+    //사이드 메뉴 show/hide
+    $('.menuButtonWrap').on('click', function(){
+        var $menuBox = $('.slideMenu');
+        $(this).toggleClass('active');
+        if($menuBox.hasClass('active')){
+            $menuBox.removeClass('active');
+        }else{
+            $menuBox.addClass('active');
+        }
+    });
+
+    //메시지 클릭 시 css애니메이션 토글
+    $('.header .message').on('click', function(){
+        $(this).toggleClass('stop');
+    });
+});
+
+//page scroll event
+$(window).scroll(function(){
+    var scTop = $(window).scrollTop();
+    if(scTop > 40){
+        $('header').addClass('minSize');
+    }else{
+        $('header').removeClass('minSize');
+    }
 
 });
 //Layer Content
@@ -7,4 +32,36 @@ function layerContShow(thisClass){
 }
 function layerContHide(thisClass){
     $('.'+thisClass).hide();
+}
+
+//page animation in
+function pageMoveBetween(){
+    setTimeout(function(){
+        $('.mask.m01').addClass('active');
+    }, 0);
+    setTimeout(function(){
+        $('.mask.m02').addClass('active');
+    }, 150);
+    setTimeout(function(){
+        $('.mask.m03').addClass('active');
+    }, 300);
+    setTimeout(function(){
+        $('.loading').addClass('active');
+    }, 450);
+}
+
+//page animation out
+function pageMoveFin(){
+    setTimeout(function(){
+        $('.loading').addClass('actived');
+    }, 0);
+    setTimeout(function(){
+        $('.mask.m03').addClass('actived');    
+    }, 150);
+    setTimeout(function(){
+        $('.mask.m02').addClass('actived');
+    }, 300);
+    setTimeout(function(){
+        $('.mask.m01').addClass('actived');
+    }, 450);
 }
